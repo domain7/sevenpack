658a5c0cc16212a1d751f0513a1124060683ae62
Reuben Moes <reuben@domain7.com>
2018-01-04 23:53:46 -0800

    modularize button twig

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import './_button.scss';
 
-$('.js-button').on('click', () => {
-  console.log('button clicked');
+$('.js-button').on('click', function(){
+  console.log('button clicked', this);
 });
