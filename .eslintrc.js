d88b16fc7e74e7430f9c717a9746aa4a63758763
Reuben Moes <reuben@domain7.com>
2018-01-04 19:16:47 -0800

    Add more eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
-    "extends": "airbnb",
-    "installedESLint": true,
-    "plugins": [
-        "import"
-    ],
-    "globals": {
-      document: true
-    },
-    "rules": {
-      "comma-dangle": 0
-    }
+  extends: 'airbnb-base',
+  rules: {
+    'no-use-before-define': 0,
+    'no-await-in-loop': 0,
+    'no-continue': 0,
+    'no-param-reassign': 1,
+    'no-constant-condition': 0,
+    'no-mixed-operators': 0,
+    'arrow-parens': 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'space-before-blocks': 0,
+    'comma-dangle': 0
+  },
+  plugins: [],
+  globals: {
+    document: true
+  },
+  env: {},
 };
